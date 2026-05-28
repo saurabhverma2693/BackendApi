a8ff701e1dc103c37eaaadf6e3feeb56d4107a2a
saurabh <saurabhverma2693@gmail.com>
2026-05-28 18:07:51 +0530

    model setup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const feedRoutes = require('./routes/feed');
 const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
@@ -12,4 +14,8 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
-app.listen(8080);
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
