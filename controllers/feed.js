508f2947790b525241e7f218c379747319356e91
saurabh <saurabhverma2693@gmail.com>
2026-05-28 15:13:48 +0530

    fetch list of post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,14 @@
 exports.getposts = (req,res,next) =>{
-    res.status(200).json({posts : [{title:'book',content: 'Physics'}]});
+    res.status(200).json({posts : [{
+        _id : "1",
+        title:'Motivation',
+        content: 'Need to be motivated to do something',
+        imageUrl:'Images/Motivation.jpg',
+        creator :{
+            name :'Saurabh Verma'
+        },
+        createdAt : new Date()
+    }]});
 }
 
 exports.createPost = (req,res,next) =>{
