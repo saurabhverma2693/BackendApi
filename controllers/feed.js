25ad41114d8ad50b84fd821b152966f97e92c9ec
saurabh <saurabhverma2693@gmail.com>
2026-05-28 15:37:11 +0530

    add a create post endpoints

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,23 +1,33 @@
-exports.getposts = (req,res,next) =>{
-    res.status(200).json({posts : [{
-        _id : "1",
-        title:'Motivation',
-        content: 'Need to be motivated to do something',
-        imageUrl:'Images/Motivation.jpg',
-        creator :{
-            name :'Saurabh Verma'
-        },
-        createdAt : new Date()
-    }]});
+exports.getposts = (req, res, next) => {
+    res.status(200).json({
+        posts: [{
+            _id: '1',
+            title: 'Motivation',
+            content: 'Need to be motivated to do something',
+            imageUrl: 'Images/Motivation.jpg',
+            creator: {
+                name: 'Saurabh Verma'
+            },
+            createdAt: new Date()
+        }]
+    });
 }
 
-exports.createPost = (req,res,next) =>{
+exports.createPost = (req, res, next) => {
 
-   const title = req.body.title;
-   const content = req.body.content;
+    const title = req.body.title;
+    const content = req.body.content;
 
     res.status(201).json({
-        Message : 'Posts created successfully......',
-        post : {id: new Date().toISOString(), title : title, content : content}
+        Message: 'Posts created successfully......',
+        post: {
+            _id: new Date().toISOString(),
+            title: title,
+            content: content,
+            creator: {
+                name: 'Saurabh Verma'
+            },
+            createdAt: new Date()
+        }
     });
 }
